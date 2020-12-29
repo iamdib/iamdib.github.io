4e505d976a4a0944caa6a9607ed7f5debd861970
iamdib01 <iamdib01@gmail.com>
2020-12-29 10:47:17 +0800

    test phase new cdn

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -45,7 +45,8 @@ if (screen.width >= 768) {
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://api.zsq.im/live2d" //"https://live2d.fghrsh.net/api/",
+			cdnPath: "https://cdn.jsdelivr.net/gh/iamdib/l2d_api@latest/"
+			//apiPath: //"https://api.zsq.im/live2d" //"https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
