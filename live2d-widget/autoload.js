9fe0c50148b6c64c48933010cbc5469d1cc46151
iamdib01 <iamdib01@gmail.com>
2021-01-20 23:31:53 +0800

    modified scripts

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -24,9 +24,9 @@ function loadExternalResource(url, type) {
 // waifu.css live2d.min.js waifu-tips.js
 if (screen.width >= 768) {
 	Promise.all([
+		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
-		loadExternalResource(live2d_path + "waifu-tips.js", "js"),
-		loadExternalResource(live2d_path + "waifu.css", "css")
+		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
