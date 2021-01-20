48b06d34e89679886ffcbab5e0e93bb478ce050b
iamdib01 <iamdib01@gmail.com>
2021-01-20 22:26:20 +0800

    optimized runtime

---
+++ assets/i18n-tr.js
@@ -0,0 +1,11 @@
+I18n.defaultLocale = "en-US";
+I18n.locale = "en-US";
+
+I18n.translations = {};
+
+fetch("/live2d-widget/i18n.json").then(res => res.json()).then(data => {
+    Object.keys(data).forEach(key => {
+        console.log("key", key)
+        I18n.translations[key] = data[key];
+    })
+});
