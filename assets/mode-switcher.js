2001e3971f8f3c24ea76b8b31b3239b208db3ca2
iamdib01 <iamdib01@gmail.com>
2021-02-03 22:01:48 +0800

    updated

--- assets/mode-switcher.js
+++ assets/mode-switcher.js
@@ -2,16 +2,17 @@ const toggleSwitch = document.getElementById('toggle');
 
 const currentTheme = localStorage.getItem("theme");
 if(currentTheme == 'dark'){      
-  document.getElementsByTagName('body')[0].classList.add('dark');
-  toggleSwitch.checked = true
+  document.getElementsByTagName('body')[0].classList.toggle('dark');
+      localStorage.getItem("theme","dark");
+      toggleSwitch.checked = true
 } else {
-  document.getElementsByTagName('body')[0].classList.remove('dark');
-  document.getElementsByTagName('body')[0].classList.add('light');
+  document.getElementsByTagName('body')[0].classList.toggle('light');
 }
 
   toggleSwitch.addEventListener("change", e => {
     if (e.target.checked) {
-      document.getElementsByTagName('body')[0].classList.add('dark');
+      document.getElementsByTagName('body')[0].classList.toggle('dark');
+      localStorage.getItem("theme","dark");
       localStorage.setItem("theme","dark");
     } else {
       document.getElementsByTagName('body')[0].classList.remove('dark');
