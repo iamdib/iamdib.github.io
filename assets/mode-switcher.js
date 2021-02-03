a3a088e11b059136bb67aa7ce8213c1f2f88e873
iamdib01 <iamdib01@gmail.com>
2021-02-03 16:34:42 +0800

    updated

--- assets/mode-switcher.js
+++ assets/mode-switcher.js
@@ -1,22 +1,21 @@
-var toggleSwitch = document.getElementById('toggle'); 	
-var currentTheme = localStorage.getItem("theme");
+const toggleSwitch = document.getElementById('toggle'); 
 
-if (currentTheme === 'dark') {
-  document.body.classList.add('dark');
+var currentTheme = localStorage.getItem("theme");
+if(currentTheme == 'light'){      
+  document.getElementsByTagName('body')[0].classList.remove('dark');
+} else {
+  document.getElementsByTagName('body')[0].classList.remove('light');
+  document.getElementsByTagName('body')[0].classList.add('dark');
   toggleSwitch.checked = true
 }
-else {
-  document.body.classList.remove('dark');
-}
-
-toggleSwitch.addEventListener("change", e => {
-  if (e.target.checked) {
-    document.body.classList.toggle('dark');
-    document.body.className = 'dark';
-    localStorage.setItem("theme","dark");
-  } else {
-    document.body.classList.toggle('light');
-    document.body.className = 'light';
-    localStorage.setItem("theme","light");
-  }
-});
+  toggleSwitch.addEventListener("change", e => {
+    if (e.target.checked) {
+      document.body.classList.toggle('dark');
+      document.body.className = 'dark';
+      localStorage.setItem("theme","dark");
+    } else {
+      document.body.classList.toggle('light');
+      document.body.className = 'light';
+      localStorage.setItem("theme","light");
+    }
+  });
