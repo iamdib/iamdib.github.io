107e6a10af432d87f4bb09ca9ee5e59fd3675cca
iamdib01 <iamdib01@gmail.com>
2021-02-03 14:20:08 +0800

    modified script

--- assets/mode-switcher.js
+++ assets/mode-switcher.js
@@ -1,20 +1,27 @@
-var toggleSwitch = document.getElementById('toggle'); 
+const toggleSwitch = document.getElementById('toggle'); 
 
-var currentTheme = localStorage.getItem("theme") || null;
+const currentTheme = localStorage.getItem("theme");
+if(currentTheme !== 'dark'){      
+  document.getElementsByTagName('body')[0].classList.add(currentTheme);
+} else {
+  document.getElementsByTagName('body')[0].classList.remove(currentTheme);
+}
 
   if (currentTheme === 'dark') {
-    document.body.className = 'dark';
+	document.body.classList.toggle('dark');
     toggleSwitch.checked = true
   }
   else {
-    document.body.className = 'light';
+    document.body.classList.toggle('light');
   }
 
   toggleSwitch.addEventListener("change", e => {
     if (e.target.checked) {
+      document.body.classList.toggle('dark');
       document.body.className = 'dark';
       localStorage.setItem("theme","dark");
     } else {
+      document.body.classList.toggle('light');
       document.body.className = 'light';
       localStorage.setItem("theme","light");
     }
